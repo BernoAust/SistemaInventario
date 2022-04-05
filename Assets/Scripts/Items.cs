1f5b8e7714dc27fa95dcbffcb7c5e16757ef6941
BernoAust <bernardotreis@gmail.com>
2022-04-05 20:43:57 -0300

    Projeto Base - 05/04
    Adicionado as pastas base do Projeto.

---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Level { get; set; }
+
+}
---
+++ Assets/Scripts/Weapons.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : Items
+{
+    
+    public int Damage { get; set; }
+    public int Health { get; set; }
+    public int Mana { get; set; }
+
+}
